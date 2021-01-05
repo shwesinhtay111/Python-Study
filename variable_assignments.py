3708f3b723751dfb62bfff10b3760c354c528dbf
ShweSinHtay <shwesin@isgm2.com>
2021-01-05 16:38:18 +0630

    variable assignment

---
+++ variable_assignments.py
@@ -0,0 +1,14 @@
+my_dogs = 2
+print(my_dogs)
+my_dogs = ['Sammy', 'Frankie']
+print(my_dogs)
+# reassigning variables
+a = 10
+a +=10
+print(a)
+a *= 4
+print(a)
+# Determining variable type with type()
+print(type(a))
+a = (2, 4)
+print(type(a))
