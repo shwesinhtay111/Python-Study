f77995b2a37166367415519d69223d235800ed3c
ShweSinHtay <shwesin@isgm2.com>
2021-01-05 13:44:12 +0630

    number study

---
+++ numbers.py
@@ -0,0 +1,30 @@
+
+# Addition
+print(2 + 1)
+# Substraction
+print(2 - 1)
+# Multiplication
+print(2 * 2)
+# Division
+print(3 / 2)
+# Floor Division
+print( 7 // 4)
+# Modulo
+print(7 % 4)
+# Powers
+print(2 ** 3)
+# Can also do roots this way
+print(4 ** 0.5)
+# Order of Operations followed in Python
+print(2 + 10 * 10 + 3)
+# Can use parentheses to specify orders
+print((2 + 10) * (10 + 3))
+# Let's create an object called 'a' and assign it the number 5
+a = 5
+a = a + a
+print(a)
+a = 20
+a = a + a
+print(a)
+
+
