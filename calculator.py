16a6785fbf901599b6a2b5b7f3adc15df5aef3de
ShweSinHtay <shwesin@isgm2.com>
2021-01-05 13:45:57 +0630

    delete test files

--- calculator.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number:"))
-result = num1 + num2
-print("Sum" + str(result))
--- eg.py
+++
@@ -1,7 +0,0 @@
-num = 10
-if num > 10:
-    print("Job will work")
-elif num == 10:
-    print("Number is 10")
-else:
-    print("Number others")
