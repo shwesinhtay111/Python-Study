ebf4bdb9019911803c4567007ab624c09efe1f6a
ShweSinHtay <shwesin@isgm2.com>
2021-01-06 16:53:34 +0630

    Set and Boolean

---
+++ set_and_boolean.py
@@ -0,0 +1,24 @@
+x = set()
+x.add(1)
+print(x)
+# Add a different element
+x.add(2)
+print(x)
+# Try to add the same element
+x.add(1)
+print(x)
+
+# Create a list with repeats
+list1 = [1,1,2,2,3,4,5,6,1,1]
+# Cast as set to get unique values
+
+print(set(list1))
+
+# Booleans
+# Set object to be a boolean
+a = True
+print(a)
+print(1 > 2)
+# None placeholder
+b = None
+print(b)
